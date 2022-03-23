411d0133fca5e9c7308ba222862a69354b309113
SimoneBr55 <39730275+SimoneBr55@users.noreply.github.com>
2022-03-23 12:49:09 +0100

    Another one

--- external_connections.sh
+++ external_connections.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #-----------USAGE----------#
-if [ "$1" == "-h" || "$1" == "--help" ]
+if [[ "$1" == "-h" || "$1" == "--help" ]]
 then
 	echo "This script has to be run directly"
 	echo "(i.e. /path/2/script OR ./scripts)"
