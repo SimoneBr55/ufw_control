d54a36a5ff7c34b7c779acbb1ef8a254b8e83d6c
SimoneBr55 <39730275+SimoneBr55@users.noreply.github.com>
2022-03-23 11:54:01 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]
+then
+	echo "Use --dry-run to see what I am doing"
+	exit 0
+fi
+
+## INSTALL SCRIPT ##
+echo "I can install two domains, right now"
+echo ""
+echo "First Domain?"
+echo ""
+read dom1
+echo "Second Domain?"
+echo ""
+read dom2
+
+FOLDER=$(pwd)
+INSTALL_LOCATION=/usr/local/sbin
+
+sudo ln -s ${FOLDER}/external_connections.sh ${INSTALL_LOCATION}/external_connections
+sudo ln -s ${FOLDER}/ufw_disable.sh ${INSTALL_LOCATION}/ufw_disable
+sudo ln -s ${FOLDER}/ufe_enable.sh ${INSTALL_LOCATION}/ufw_enable
+
+echo "Installing crontab as:"
+echo "@reboot ${INSTALL_LOCATION}/external_connections ${dom1} ${dom2}"
+sudo /bin/bash -c 'echo "@reboot root ${INSTALL_LOCATION}/external_connections ${dom1} ${dom2}" >> /etc/crontab'
+
+exit 0
